71afab3044ac6d9facab6f4aeaba1ab0e6ecadd7
Dimas Rifqi <dimasrifqi296@gmail.com>
2019-01-09 15:54:04 +0700

    change route default

---
+++ application/views/dashboard.php
@@ -0,0 +1,22 @@
+<?php $this->load->view('layouts/header.php'); ?>
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <section class="content-header">
+        <h1>
+          Dashboard
+        </h1>
+        <ol class="breadcrumb">
+          <li><a href="<?php echo base_url(); ?>"><i class="fa fa-dashboard"></i> Home</a></li>
+          <li class="active">Dashboard</li>
+        </ol>
+      </section>
+
+      <!-- Main content -->
+      <section class="content">
+          
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+    <?php $this->load->view('layouts/footer.php'); ?>
